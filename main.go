261e43410f03802b1380bb400a06bdec042dc29d
Chisugi Kokonoe <41792049+YKMeIz@users.noreply.github.com>
2019-10-30 08:07:15 +1000

    first commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+)
+
+func main() {
+	virtualHosts, err := getVirtualHosts()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// redirect www to non-www
+		if strings.HasPrefix(r.Host, "www") {
+			target := "https://" + r.Host[4:] + r.URL.Path
+			if len(r.URL.RawQuery) > 0 {
+				target += "?" + r.URL.RawQuery
+			}
+			http.Redirect(w, r, target, http.StatusFound)
+			return
+		}
+
+		ip, ok := virtualHosts.Load(strings.Split(r.Host, ":")[0])
+
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		director := func(req *http.Request) {
+			req.Header = r.Header
+			req.URL = r.URL
+			req.URL.Scheme = "http"
+			req.URL.Host = ip.(string)
+		}
+		proxy := &httputil.ReverseProxy{Director: director}
+		proxy.ServeHTTP(w, r)
+	})
+
+	log.Fatal(http.ListenAndServeTLS(":443", "server.crt", "server.key", mux))
+}
